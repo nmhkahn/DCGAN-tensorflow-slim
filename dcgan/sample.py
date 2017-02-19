39b24b9c6a3e7955cd0fcc5444df7ac96ce74729
NamHyuk Ahn <nmhkahn@gmail.com>
2017-02-19 23:26:33 +0900

    Fix errors

--- dcgan/sample.py
+++ dcgan/sample.py
@@ -1,3 +1,4 @@
+import os
 import scipy.misc
 import numpy as np
 
@@ -6,16 +7,17 @@ import config
 import trainer
 
 def main():
-    cfg = config.Config(filename_queue="dataset/flowers.tfrecords")
+    cfg = config.Config(filename_queue="dataset/flowers.tfrecords",
+                        logdir="log_flowers")
     t = trainer.Trainer(cfg)
 
     if not os.path.exists(cfg.sampledir):
         os.makedirs(cfg.sampledir)
 
-    _, im = t.sample(100)
-    for i in range(100):
-        imname = os.path.join(config.sampledir, str(step+1)+".jpg")
-        scipy.misc.imsave("example/"+str(i+1)+".jpg", im[i])
+    _, im = t.sample(20)
+    for i in range(20):
+        imname = os.path.join(cfg.sampledir, str(i+1)+".jpg")
+        scipy.misc.imsave(imname, im[i])
 
 if __name__ == "__main__":
     main()
